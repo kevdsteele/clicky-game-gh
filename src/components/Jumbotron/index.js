b126e1b2bc1d40e940b52642c748479d28974406
Kevin Steele <kevdsteele@gmail.com>
2019-09-17 22:54:36 -0400

    tweak image issue

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -2,9 +2,14 @@ import React from "react";
 
 import "./style.css"
 
+const divStyle = {
+ 
+  backgroundImage: 'url(jumbotron.jpeg)',
+};
+
 function Jumbotron() {
   return (
-    <div className="jumbotron" style="background-image: url(jumbotron.jpeg)">
+    <div className="jumbotron" style={divStyle}>
       <h1 className="text-center pb-2">Clicky Game!</h1>
       <p className="text-center">
       Can you remember which images you have clicked? Try our Clicky Memory Game. You can only click each image one time to earn a point. 
