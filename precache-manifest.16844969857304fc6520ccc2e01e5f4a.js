1c8b206b5e5e2d7c02de4a7b6c0f477378009b65
Kevin Steele <kevdsteele@gmail.com>
2019-09-17 22:37:09 -0400

    Updates

---
+++ precache-manifest.16844969857304fc6520ccc2e01e5f4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7348de7050914408a8d1aa739eb0487",
+    "url": "/clicky-game-gh/index.html"
+  },
+  {
+    "revision": "c38c83b7df4750a6e4cb",
+    "url": "/clicky-game-gh/static/css/main.5efa09ef.chunk.css"
+  },
+  {
+    "revision": "1cb666807e2f5de35a59",
+    "url": "/clicky-game-gh/static/js/2.15641e16.chunk.js"
+  },
+  {
+    "revision": "c38c83b7df4750a6e4cb",
+    "url": "/clicky-game-gh/static/js/main.08dc6de3.chunk.js"
+  },
+  {
+    "revision": "1f8d6d3661f750fce22e",
+    "url": "/clicky-game-gh/static/js/runtime~main.e46db2dc.js"
+  }
+]);
